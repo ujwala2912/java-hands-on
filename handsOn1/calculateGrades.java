f022f9cfa8d92a93d6d76786774e2b3dfd6a49f1
ujwala <ujju.16@gmail.com>
2015-10-08 17:46:37 +0100

    first commit

---
+++ handsOn1/calculateGrades.java
@@ -0,0 +1,36 @@
+package handsOn1;
+
+import java.util.Scanner;
+
+public class calculateGrades {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		subjects sub = new subjects();
+
+		System.out.println("Enter the name of the Student");
+		sub.setName(in.nextLine());
+
+		System.out.println("Enter Mathematics marks");
+		sub.setMathematicsMarks(in.nextInt());
+
+		System.out.println("Enter English marks");
+		sub.setEnglishMarks(in.nextInt());
+
+		System.out.println("Enter Hindi marks");
+		sub.setHindiMarks(in.nextInt());
+
+		System.out.println("Enter Science marks");
+		sub.setScienceMarks(in.nextInt());
+
+		System.out.println("Enter Social Science marks");
+		sub.setSocialMarks(in.nextInt());
+
+		System.out.println(sub.getName() + "'s percentage is " + sub.calculatePercentage() + "% and the Category is "
+				+ sub.getGrade());
+
+	}
+
+}
---
+++ handsOn1/subjects.java
@@ -0,0 +1,96 @@
+package handsOn1;
+
+public class subjects {
+
+	private String name;
+	private int mathematicsMarks;
+	private int englishMarks;
+	private int hindiMarks;
+	private int scienceMarks;
+	private int socialMarks;
+
+	@Override
+	public String toString() {
+		return "subjects [name=" + name + ", mathematicsMarks=" + mathematicsMarks + ", englishMarks=" + englishMarks
+				+ ", hindiMarks=" + hindiMarks + ", scienceMarks=" + scienceMarks + ", socialMarks=" + socialMarks
+				+ "]";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMathematicsMarks() {
+		return mathematicsMarks;
+	}
+
+	public void setMathematicsMarks(int mathematicsMarks) {
+		this.mathematicsMarks = mathematicsMarks;
+	}
+
+	public int getEnglishMarks() {
+		return englishMarks;
+	}
+
+	public void setEnglishMarks(int englishMarks) {
+		this.englishMarks = englishMarks;
+	}
+
+	public int getHindiMarks() {
+		return hindiMarks;
+	}
+
+	public void setHindiMarks(int hindiMarks) {
+		this.hindiMarks = hindiMarks;
+	}
+
+	public int getScienceMarks() {
+		return scienceMarks;
+	}
+
+	public void setScienceMarks(int scienceMarks) {
+		this.scienceMarks = scienceMarks;
+	}
+
+	public int getSocialMarks() {
+		return socialMarks;
+	}
+
+	public void setSocialMarks(int socialMarks) {
+		this.socialMarks = socialMarks;
+	}
+
+	public double calculatePercentage() {
+		double totalMarks;
+
+		totalMarks = getMathematicsMarks() + getEnglishMarks() + getHindiMarks() + getScienceMarks() + getSocialMarks();
+
+		return (totalMarks) / 5;
+
+	}
+
+	public String getGrade() {
+
+		String grade;
+		if (calculatePercentage() < 40)
+			grade = "Poor";
+		else if (calculatePercentage() >= 40 && calculatePercentage() < 60)
+			grade = "Average";
+		else if (calculatePercentage() >= 60 && calculatePercentage() < 80)
+			grade = "Good";
+		else if (calculatePercentage() >= 80 && calculatePercentage() < 90)
+			grade = "Very good";
+		else if (calculatePercentage() >= 90)
+			grade = "Excellent";
+		else
+			grade = "Unknown";
+
+		return grade;
+
+	}
+
+}
